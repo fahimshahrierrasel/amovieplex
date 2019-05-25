481cee8c90fefad9b3b3a54f93e89f7f7a4e586b
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2019-05-25 16:25:44 +0600

    movie media index add with ajax request

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,17 +3,36 @@ from . import views
 
 urlpatterns = [
     # Auth
-    path('login', views.LoginView.as_view(), name='dashboard.login'),
-    path('logout', views.LogoutView.as_view(), name='dashboard.logout'),
+    path("login", views.LoginView.as_view(), name="dashboard.login"),
+    path("logout", views.LogoutView.as_view(), name="dashboard.logout"),
     # Dashboard
-    path('', views.DashboardView.as_view(), name='dashboard.index'),
+    path("", views.DashboardView.as_view(), name="dashboard.index"),
     # Movies
-    path('movies/', views.MovieListView.as_view(),
-         name='dashboard.movie.list'),
-    path('movies/<int:pk>', views.MovieDetailView.as_view(),
-         name='dashboard.movie.detail'),
-    path('movies/create', views.MovieCreateView.as_view(),
-         name='dashboard.movie.create'),
-    path('movies/<int:pk>/edit', views.MovieUpdateView.as_view(),
-         name='dashboard.movie.edit'),
+    path("movies/", views.MovieListView.as_view(), name="dashboard.movie.list"),
+    path(
+        "movies/<int:pk>",
+        views.MovieDetailView.as_view(),
+        name="dashboard.movie.detail",
+    ),
+    path(
+        "movies/create",
+        views.MovieCreateView.as_view(),
+        name="dashboard.movie.create",
+    ),
+    path(
+        "movies/<int:pk>/edit",
+        views.MovieUpdateView.as_view(),
+        name="dashboard.movie.edit",
+    ),
+    # Movie Medias
+    path(
+        "movie-medias",
+        views.MovieMediaView.as_view(),
+        name="dashboard.movie_medias",
+    ),
+    path(
+        "movie-medias/<int:movie_id>",
+        views.medias_of_movie,
+        name="dashboard.medias"
+    )
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,55 +4,62 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormMixin
 from django.views.generic import (
-    ListView, DetailView, CreateView, UpdateView, DeleteView)
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import redirect
 from movie import models as movie_models
 from . import forms
+from django.http import HttpResponse
+from django.core import serializers
 
 
 class LoginView(LoginView):
-    template_name = 'dashboard/auth/login.html'
+    template_name = "dashboard/auth/login.html"
     redirect_authenticated_user = True
-    extra_context = {'page_title': 'Admin Login'}
+    extra_context = {"page_title": "Admin Login"}
 
     def get_success_url(self):
         url = self.get_redirect_url()
-        return url or reverse('dashboard.index')
+        return url or reverse("dashboard.index")
 
 
 class LogoutView(LogoutView):
-    next_page = '/admin/dashboard/login'
+    next_page = "/admin/dashboard/login"
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
-    login_url = '/admin/dashboard/login'
-    extra_context = {'page_title': 'Dashboard'}
+    login_url = "/admin/dashboard/login"
+    extra_context = {"page_title": "Dashboard"}
     template_name = "dashboard/index.html"
 
 
 class MovieListView(LoginRequiredMixin, ListView):
-    login_url = '/admin/dashboard/login'
-    extra_context = {'page_title': 'Movies'}
-    template_name = 'dashboard/movies/movie_list.html'
+    login_url = "/admin/dashboard/login"
+    extra_context = {"page_title": "Movies"}
+    template_name = "dashboard/movies/movie_list.html"
     model = movie_models.Movie
-    context_object_name = 'movies'
+    context_object_name = "movies"
 
 
 class MovieDetailView(LoginRequiredMixin, FormMixin, DetailView):
     model = movie_models.Movie
-    template_name = 'dashboard/movies/movie_detail.html'
-    extra_context = {'page_title': 'Movie Detail'}
+    template_name = "dashboard/movies/movie_detail.html"
+    extra_context = {"page_title": "Movie Detail"}
     form_class = forms.MovieMediaForm
 
     def get_success_url(self):
-        return reverse('dashboard.movie.detail', kwargs={'pk': self.object.pk})
+        return reverse("dashboard.movie.detail", kwargs={"pk": self.object.pk})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         media_form = self.get_form()
-        media_form.fields['movie'].initial = self.object.pk
-        context['form'] = media_form
+        media_form.fields["movie"].initial = self.object.pk
+        context["form"] = media_form
         return context
 
     def post(self, request, *args, **kwargs):
@@ -71,17 +78,34 @@ class MovieDetailView(LoginRequiredMixin, FormMixin, DetailView):
 
 
 class MovieCreateView(LoginRequiredMixin, CreateView):
-    login_url = '/admin/dashboard/login'
-    extra_context = {'page_title': 'New Movie'}
+    login_url = "/admin/dashboard/login"
+    extra_context = {"page_title": "New Movie"}
     model = movie_models.Movie
     form_class = forms.MovieForm
-    template_name = 'dashboard/movies/movie_form.html'
+    template_name = "dashboard/movies/movie_form.html"
 
 
 class MovieUpdateView(LoginRequiredMixin, UpdateView):
-    login_url = '/admin/dashboard/login'
-    extra_context = {'page_title': 'Update Movie'}
+    login_url = "/admin/dashboard/login"
+    extra_context = {"page_title": "Update Movie"}
     model = movie_models.Movie
     form_class = forms.MovieForm
-    template_name = 'dashboard/movies/movie_form.html'
-    success_url = reverse_lazy('dashboard.movie.list')
+    template_name = "dashboard/movies/movie_form.html"
+    success_url = reverse_lazy("dashboard.movie.list")
+
+
+class MovieMediaView(LoginRequiredMixin, TemplateView):
+    login_url = "/admin/dashboard/login"
+    extra_context = {"page_title": "Movie Medias"}
+    template_name = "dashboard/movie_medias/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["movies"] = movie_models.Movie.objects.all()
+        return context
+
+
+def medias_of_movie(request, movie_id):
+    movie_medias = movie_models.MovieMedia.objects.filter(movie=movie_id)
+    data = serializers.serialize('json', movie_medias)
+    return HttpResponse(data, content_type='application/json')
